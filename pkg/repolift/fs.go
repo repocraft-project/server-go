87b671e60eb41c11067ec92572651a35c3c005b8
Cylix Lee <cylix.lee@outlook.com>
2026-03-04 16:11:56 +0800

    refactor(repolift): Use simplified FS to support local filesystem, KV Cache, and other remote storage.

---
+++ pkg/repolift/fs.go
@@ -0,0 +1,22 @@
+package repolift
+
+import "io"
+
+type ReadonlyFile interface {
+	io.Reader
+	io.Seeker
+	io.Closer
+}
+
+type WriteonlyFile interface {
+	io.Writer
+	io.Seeker
+	io.Closer
+}
+
+type FS interface {
+	Create(entry string) error
+	Reader(entry string) (ReadonlyFile, error)
+	Writer(entry string) (WriteonlyFile, error)
+	Delete(entry string) error
+}
